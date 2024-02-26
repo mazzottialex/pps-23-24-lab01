605c75d4c20bb5adb352f7305de67898b644407a
alexmazzotti <alex.mazzotti2@studio.unibo.it>
2024-02-26 10:52:52 +0100

    Create class SimpleBankAccountWithAtmTest

---
+++ basic-refactoring-exercise/src/main/java/example/model/SimpleBankAccountWithAtm.java
@@ -0,0 +1,29 @@
+package example.model;
+
+public class SimpleBankAccountWithAtm implements BankAccount {
+
+    @Override
+    public AccountHolder getHolder() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getHolder'");
+    }
+
+    @Override
+    public double getBalance() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getBalance'");
+    }
+
+    @Override
+    public void deposit(int userID, double amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deposit'");
+    }
+
+    @Override
+    public void withdraw(int userID, double amount) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'withdraw'");
+    }
+
+}
---
+++ basic-refactoring-exercise/src/test/java/SimpleBankAccountWithAtmTest.java
@@ -0,0 +1,12 @@
+import org.junit.jupiter.api.Test;
+
+import example.model.BankAccount;
+import example.model.SimpleBankAccountWithAtm;
+
+class SimpleBankAccountWithAtmTest {
+    
+    @Test
+    void CreateSimpleBankAccountWithAtm(){
+        BankAccount simpleBankAccountWithAtm= new SimpleBankAccountWithAtm();
+    }
+}
